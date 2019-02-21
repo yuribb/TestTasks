813beecfe18bc42f9136a28226b475ea56eae115
Yuri Bb <iamyuri@bk.ru>
2019-02-21 04:26:34 +0300

    Add files via upload

---
+++ TestRegistryService/TestRegistryService/Core/RegeditEditor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace TestRegistryService.Core
+{
+    public static class RegeditEditor
+    {
+        public static bool IsAdmin()
+        {
+            bool isAdmin = new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+            return isAdmin;
+        }
+
+        public static string AddRegistryKey(string companyName, string productName, string registryKeyName, string registryKeyValue)
+        {
+            if (string.IsNullOrEmpty(companyName)) throw new ArgumentNullException(nameof(companyName));
+            if (string.IsNullOrEmpty(productName)) throw new ArgumentNullException(nameof(productName));
+            if (string.IsNullOrEmpty(registryKeyName)) throw new ArgumentNullException(nameof(registryKeyName));
+            if (string.IsNullOrEmpty(registryKeyValue)) throw new ArgumentNullException(nameof(registryKeyValue));
+
+            RegistryKey key = Registry.LocalMachine;
+            key = key.CreateSubKey("SOFTWARE");
+            key = key.CreateSubKey(companyName);
+            key = key.CreateSubKey(productName);
+            key.SetValue(registryKeyName, registryKeyValue);
+            return key.ToString();
+        }
+
+        public static bool ChangePermissionToUser(string userName, string registryKey, RegistryRights registryRights = RegistryRights.ReadKey)
+        {
+            string user = userName;
+            RegistrySecurity rs = new RegistrySecurity();
+
+            rs.AddAccessRule(new RegistryAccessRule(user,
+                registryRights,
+                InheritanceFlags.None,
+                PropagationFlags.None,
+                AccessControlType.Allow));
+            string rootKey = registryKey.Replace($"{Registry.LocalMachine.ToString()}\\", string.Empty);
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(rootKey, true);
+
+            try
+            {
+                key.SetAccessControl(rs);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ TestRegistryService/TestRegistryService/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TestRegistryService.BaseService;
+using TestRegistryService.Service;
+
+namespace TestRegistryService
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.File("service.log").CreateLogger();
+            bool isService = !(Debugger.IsAttached || args.Contains("-console"));
+
+            try
+            {
+                Log.Logger.Debug("Creating service");
+                IHostBuilder builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<RegistryService>();
+                });
+
+                if (isService)
+                {
+                    Log.Logger.Information("Starting service");
+                    await builder.RunAsServiceAsync();
+                }
+                else
+                {
+                    Console.WriteLine("Start console app");
+                    await builder.RunConsoleAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error($"{ex.Message}\r\n{ex.StackTrace}");
+            }
+        }
+    }
+}
